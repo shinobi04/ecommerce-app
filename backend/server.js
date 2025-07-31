2215df7f795d1551f92f1d6084b21d9b197f7cae
Anurag Kumar Singh <anuragkrsingh3456@gmail.com>
2025-07-31 19:23:37 +0530

    working on product model

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 import initDB from "./lib/db.js";
 import { errorHandler } from "./lib/handlers.js";
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/product", productRoutes);
 
 app.use(errorHandler);
 
